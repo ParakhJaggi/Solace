2d5d5a2c89476133742ccc5c39e159db0c2f0609
ParakhJaggi <parakhjaggi@gmail.com>
2025-10-27 16:51:11 -0400

    add tags

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,20 +1,46 @@
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata = {
-  title: 'Solace - Find Comfort in Scripture',
-  description: 'Share what you\'re going through and receive comforting Bible verses with heartfelt guidance.',
-  keywords: 'bible, verses, comfort, encouragement, faith, spiritual support',
+  title: 'Solace - Find Comfort in the Texts You Love',
+  description: 'Share what you\'re going through and receive comforting passages from the Bible or Harry Potter with heartfelt guidance.',
+  keywords: 'bible, verses, comfort, encouragement, faith, spiritual support, harry potter, wisdom',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-THDRHT2G');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+        
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
       </head>
-      <body>{children}</body>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-THDRHT2G"
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
+        {children}
+      </body>
     </html>
   )
 }
